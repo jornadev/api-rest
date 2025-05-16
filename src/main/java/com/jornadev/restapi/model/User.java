c849409b80c5c479877b1580dfba8b5125ba7276
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-05-16 09:05:29 -0300

    feat: implementing model User

--- src/main/java/com/jornadev/restapi/model/User.java
+++ src/main/java/com/jornadev/restapi/model/User.java
@@ -1,4 +1,40 @@
 package com.jornadev.restapi.model;
 
 public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public User(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public User (){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
