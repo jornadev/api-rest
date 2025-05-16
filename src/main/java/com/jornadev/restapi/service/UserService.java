1e5ee1291452c762eef3d15b081f1be686a135d3
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-05-16 09:28:59 -0300

    feat: implementing UserService

--- src/main/java/com/jornadev/restapi/service/UserService.java
+++ src/main/java/com/jornadev/restapi/service/UserService.java
@@ -1,4 +1,40 @@
 package com.jornadev.restapi.service;
 
+import com.jornadev.restapi.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
 public class UserService {
+
+    private final Map<Long, User> users = new HashMap<>();
+    private final AtomicLong idCounter = new AtomicLong();
+
+    public List<User> getAllUsers(){
+        return new ArrayList<>(users.values());
+    }
+
+    public User createUser(User user) {
+        long id = idCounter.getAndIncrement();
+        user.setId(id);
+        users.put(id, user);
+        return user;
+    }
+
+    public User updateUser(Long id,User user) {
+        users.containsKey(id);
+            user.setId(id);
+            users.put(id, user);
+            return user;
+    }
+
+    public void deleteUser(Long id) {
+        users.remove(id);
+    }
 }
+
