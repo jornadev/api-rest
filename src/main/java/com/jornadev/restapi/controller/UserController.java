f12145c5c24597ca862e80852618e9ddd73bd617
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-05-16 10:02:11 -0300

    feat: implementing UserController

--- src/main/java/com/jornadev/restapi/controller/UserController.java
+++ src/main/java/com/jornadev/restapi/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.jornadev.restapi.controller;
 
+import com.jornadev.restapi.model.User;
+import com.jornadev.restapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
 }
